3c140ea23ba83d9f27e6b38eb63c52a8a8168bd5
DzianisMatveyenka <dzianis.matveyenka@mail.ru>
2019-03-21 11:59:24 +0300

    at01-at04 complete

---
+++ src/by/it/akhmelev/at01/Main.java
@@ -0,0 +1,13 @@
+package by.it.akhmelev.at01;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Hello hello=new Hello();
+        hello.setSlogan("Привет мир");
+        hello.printSlogan();
+        hello.setSlogan("Hello 2");
+        hello.printSlogan();
+    }
+
+}
