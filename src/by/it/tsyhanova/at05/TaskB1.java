d4bf7abdc42d62aee1d57366b899fb4d5088aab2
TatsianaTsyhanova <tatsianatsyhanova@yahoo.com>
2019-03-22 14:31:30 +0300

    at05 TaskA1-TaskA2 complete TaskB1 complete TaskB2 sort and delete punctuation complete

---
+++ src/by/it/tsyhanova/at05/TaskB1.java
@@ -0,0 +1,66 @@
+package by.it.tsyhanova.at05;
+
+
+public class TaskB1 {
+    public static void main(String[] args) {
+        char[] vowels=new char[]{'а','е','ё','и','о','у','ы','э','ю','я','А','Е','Ё','И','О','У','Ы','Э','Ю','Я'};
+        char[] consonants=new char[]{'б','в','г','д','ж','з','й','к','л','м','н','п','р','с','т','ф','х','ц','ч','ш','щ'};
+        char[] delimiters = new char[]{'.', ',', ' ', '?', '!', '-', '\t', '\n', '\r'};
+        //создаем билдер для получения возможности изменять строку
+        StringBuilder sb=new StringBuilder(Poem.text);
+        //Вывести в консоль все слова текста, начинающихся согласной и
+        // заканчивающихся гласной буквой
+        // (нужно сделать для проверки этого условия приватный метод без regex)
+        int lexemeCount = 0;
+        int lastLexemeStart = 0;
+        for (int i = 0; i < sb.length(); i++) {
+            if (charIsDelimiter(sb.charAt(i), delimiters)) {
+                if (i - lastLexemeStart > 0) {
+                    lexemeCount++;
+                }
+                lastLexemeStart = i + 1;
+            } else if (i == sb.length() - 1) {
+                lexemeCount++;
+            }
+        }
+
+        String[] lexeme = new String[lexemeCount];
+        lexemeCount = 0;
+        lastLexemeStart = 0;
+        for (int i = 0; i < sb.length(); i++) {
+            if (charIsDelimiter(sb.charAt(i), delimiters)) {
+                if (i - lastLexemeStart > 0) {
+                    lexeme[lexemeCount] = sb.substring(lastLexemeStart, i);
+                    lexemeCount++;
+                }
+                lastLexemeStart = i + 1;
+            } else if (i == sb.length() - 1) {
+                lexeme[lexemeCount] = sb.substring(lastLexemeStart, i + 1);
+            }
+        }
+
+        for (int i = 0; i < lexeme.length; i++) {
+            boolean searchWord=false;
+            for(int j=0;j<vowels.length;j++){
+                if (lexeme[i].charAt(0) ==vowels[j]) {
+                    searchWord=true;
+                }
+            }
+            if(searchWord){
+                for(int j=0;j<consonants.length;j++){
+                    if (lexeme[i].charAt(lexeme[i].length()-1) ==consonants[j]){
+                        System.out.println(lexeme[i]);
+                    }
+                }
+            }
+        }
+    }
+    private static boolean charIsDelimiter(char c, char[] delimiters) {
+            for (int i = 0; i < delimiters.length; i++) {
+                if (c == delimiters[i]) {
+                    return true;
+                }
+            }
+            return false;
+    }
+}
---
+++ src/by/it/tsyhanova/at05/TaskB2.java
@@ -0,0 +1,43 @@
+package by.it.tsyhanova.at05;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TaskB2 {
+
+    public static void main(String[] args) {
+        //Вывести через \n все предложения текста в порядке возрастания
+        // количества символов (!) в каждом из них. В предложениях нужно
+        // сначала заменить все небуквенные символы и их последовательности
+        // на один пробел и выполнить trim() для каждого предложения.
+        StringBuilder sb=new StringBuilder(Poem.text);
+
+        Pattern pat0= Pattern.compile("[;:\\-\\,]");
+        Matcher matcher0=pat0.matcher(sb);
+        while (matcher0.find()) {
+            int start = matcher0.start();
+            sb.setCharAt(start, ' ');
+        }
+
+        Pattern pat1 = Pattern.compile("(\\s){2,}");
+        Matcher matcher1=pat1.matcher(sb);
+        while (matcher1.find()) {
+            int start = matcher1.start();
+        }
+
+        Pattern pattern = Pattern.compile("[\\.!?]");
+        String[] sent = pattern.split(sb);
+        for(int i=0;i<sent.length;i++){
+            for(int j=0;j<sent.length;j++){
+                if(sent[i].length()<sent[j].length()){
+                    String temp=sent[i];
+                    sent[i]=sent[j];
+                    sent[j]=temp;
+                }
+            }
+        }
+        for(String element:sent){
+            System.out.println(element.trim()+'\n');
+        }
+    }
+}
