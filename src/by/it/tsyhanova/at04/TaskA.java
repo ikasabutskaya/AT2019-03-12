45df2817b23c31b90197f235542f1d7a0d0de936
TatsianaTsyhanova <tatsianatsyhanova@yahoo.com>
2019-03-23 16:56:58 +0300

    at01-at05

--- src/by/it/tsyhanova/at04/TaskA.java
+++ src/by/it/tsyhanova/at04/TaskA.java
@@ -3,13 +3,7 @@ package by.it.tsyhanova.at04;
 import java.util.Scanner;
 
 public class TaskA {
-    public static void main(String[] args){
-        printMulTable();
-        Scanner scanner=new Scanner(System.in);
-        String str=scanner.nextLine();
-        buildOneDimArray(str);
 
-    }
     // Таблица умножения от 2 до 9 включительно
     private static void printMulTable(){
         for(int i=2;i<10;i++){
@@ -18,17 +12,17 @@ public class TaskA {
             }
             System.out.println();
         }
-        System.out.println("\n"+"Напечатайте 10 произвольных чисел через пробел");
+
     }
 
 
-    private static void buildOneDimArray(String line){
+    static void buildOneDimArray(String line){
         //получение массива вещественных чисел  из строки класса InOut
         double[] array=InOut.getArray(line);
+        double start=array[0];
+        double stop=array[array.length-1];
         //распечатка массива по 5 элементов в строке
         InOut.printArray(array,"V",5);
-        double first=array[0];
-        double last=array[array.length-1];
         //отсортировать массив
         Helper.sort(array);
         //вывести отсортированный массив в 4 колонки
@@ -36,18 +30,26 @@ public class TaskA {
         //выполнить вычисление новых (после сортировки) индексов первого
         // и последнего элемента исходного массива и напечатать их
         for(int i=0;i<array.length;i++){
-            if(array[i]==first){
+            if(array[i]==start){
                 System.out.println("Index of first element="+i);
                 break;
             }
         }
         for(int i=0;i<array.length;i++){
-            if(array[i]==last){
+            if(array[i]==stop){
                 System.out.println("Index of last element="+i);
                 break;
             }
         }
         System.out.println();
+    }
+
+    public static void main(String[] args){
+        printMulTable();
+        Scanner scan=new Scanner(System.in);
+        String s=scan.nextLine();
+        buildOneDimArray(s);
+
     }
 
 }
--- src/by/it/tsyhanova/at05/TaskB2.java
+++ src/by/it/tsyhanova/at05/TaskB2.java
@@ -1,7 +1,7 @@
 package by.it.tsyhanova.at05;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class TaskB2 {
 
@@ -10,34 +10,28 @@ public class TaskB2 {
         // количества символов (!) в каждом из них. В предложениях нужно
         // сначала заменить все небуквенные символы и их последовательности
         // на один пробел и выполнить trim() для каждого предложения.
-        StringBuilder sb=new StringBuilder(Poem.text);
 
-        Pattern pat0= Pattern.compile("[;:\\-\\,]");
-        Matcher matcher0=pat0.matcher(sb);
-        while (matcher0.find()) {
-            int start = matcher0.start();
-            sb.setCharAt(start, ' ');
+        //разделяем текст на предложения, используя знаки окончания приедложения
+        //с помощью метода replace заменяем многоточие на пробел
+        String[] sentences=Poem.text.replace("..."," ").split("[.?!]");
+        for (int i = 0; i <sentences.length ; i++) {
+            //[^а-яА-ЯёЁ]+ все символы, кроме буквенных
+            //методом replaceAll заменяем все небуквенные символы на пробел
+            // обрезаем пробел в начале и конце строки методом trim
+            sentences[i]=sentences[i].replaceAll("[^а-яА-ЯёЁ]+"," ").trim();
         }
-
-        Pattern pat1 = Pattern.compile("(\\s){2,}");
-        Matcher matcher1=pat1.matcher(sb);
-        while (matcher1.find()) {
-            int start = matcher1.start();
-        }
-
-        Pattern pattern = Pattern.compile("[\\.!?]");
-        String[] sent = pattern.split(sb);
-        for(int i=0;i<sent.length;i++){
-            for(int j=0;j<sent.length;j++){
-                if(sent[i].length()<sent[j].length()){
-                    String temp=sent[i];
-                    sent[i]=sent[j];
-                    sent[j]=temp;
+        //Сортировка массива строк с помощью компаратора
+        for (int i = 0; i <sentences.length ; i++) {
+            for (int j = 0; j <sentences.length ; j++){
+                if(sentences[i].length()<sentences[j].length()){
+                    String str=sentences[i];
+                    sentences[i]=sentences[j];
+                    sentences[j]=str;
                 }
             }
         }
-        for(String element:sent){
-            System.out.println(element.trim()+'\n');
+        for(String element:sentences){
+            System.out.println(element);
         }
     }
 }
