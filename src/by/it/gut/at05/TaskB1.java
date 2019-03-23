7a9b53ac3233620ce2f7d1e694745553154e8860
dmitry <gutik88@gmail.com>
2019-03-23 12:22:37 +0300

    at05_B1 by Dmitry Gut

--- src/by/it/gut/at05/TaskB1.java
+++ src/by/it/gut/at05/TaskB1.java
@@ -1,11 +1,28 @@
-/*
 package by.it.gut.at05;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TaskB1 {
+    public static void main(String[] args) {
+        Pattern p = Pattern.compile("[а-яА-ЯёЁ]+");
+            Matcher matcher = p.matcher(Poem.text);
+            while (matcher.find()) {
+                String word = matcher.group();
+                if (check(word))
+                    System.out.println(word);
+            }
+        }
+        private static final String vow = "аАоОуУэЭыяЯёЁюЮеЕиИ";
 
+        private static boolean check (String word){
+            boolean first = vow.indexOf(word.charAt(0))<0;
+
+
+        }
+    }
+}
+/*
 
     //получить текст
     StringBuilder sb = new StringBuilder(Poem.text);
