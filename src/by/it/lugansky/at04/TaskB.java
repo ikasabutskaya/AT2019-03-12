6ad68429fda81e225c6ec357cf31581f7bc54fb2
droffden <romanlugansky@gmail.com>
2019-03-23 11:22:01 +0300

    at05-TaskA

--- src/by/it/lugansky/at04/TaskB.java
+++ src/by/it/lugansky/at04/TaskB.java
@@ -1,33 +1,33 @@
 package by.it.lugansky.at04;
 
-import java.util.Scanner;
+            import java.util.Scanner;
 
 public class TaskB {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-            int people = scanner.nextInt();
-            scanner.nextInt();
+        int people = scanner.nextInt();
+        scanner.nextInt();
 
-            // getting names for n people
+        // getting names for n people
 
-            String[] names = new String[people];
-            for (int i = 0; i < people; i++) {
-                names[i] = scanner.nextLine();
-            }
-
-
-            // getting salary for each person for each quarter
-
-            double[][] salary = new double[people][4];
-            for (int i = 0; i < people; i++) {
-                System.out.println("Введите зарплату для " + names[i]);
-                double[] personSalary = InOut.getArray(scanner.nextLine());
-                salary[i] = personSalary;
-            }
+        String[] names = new String[people];
+        for (int i = 0; i < people; i++) {
+            names[i] = scanner.nextLine();
         }
-        // calc metrics
 
 
+        // getting salary for each person for each quarter
+
+        double[][] salary = new double[people][4];
+        for (int i = 0; i < people; i++) {
+            System.out.println("Введите зарплату для " + names[i]);
+            double[] personSalary = InOut.getArray(scanner.nextLine());
+            salary[i] = personSalary;
+        }
     }
+    // calc metrics
+
+
+}
