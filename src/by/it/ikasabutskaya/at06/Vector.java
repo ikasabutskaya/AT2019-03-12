a13154fa546b55866484e226f721375b022a3345
ikasabutskaya <ikasabutskaya@gmail.com>
2019-03-26 02:12:18 +0300

    Task 6

---
+++ src/by/it/ikasabutskaya/at06/Vector.java
@@ -0,0 +1,30 @@
+package by.it.ikasabutskaya.at06;
+
+import java.util.Arrays;
+
+class Vector extends Var{
+
+/* ошибка, поправить завтра
+
+ */
+/*    private double[] value;
+
+    Vector(double[] value) {
+        this.value = value;
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb=new StringBuilder("{");
+        String delimiter="";
+        for (double element : value) {
+            sb.append(delimiter).append(element);
+            delimiter=", ";
+        }
+        sb.append("}");
+        return sb.toString();
+    }*/
+
+
+}
