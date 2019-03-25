1f58c71fcf33ec144dfdc84efdde28f65ea4ee07
Vitali Shchur <vitali.shchur@gmail.com>
2019-03-25 23:56:17 +0300

    AT05 commit

---
+++ src/by/it/shchur/AT05/TaskB2.java
@@ -0,0 +1,26 @@
+package by.it.shchur.AT05;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class TaskB2 {
+    public static void main(String[] args) {
+       // System.out.print(Poem.text);
+        String[] sentences = Poem.text.split("[.?!][^?=.{3,}]");
+        for (int i = 0; i < sentences.length; i++) {
+            sentences[i]=sentences[i].replaceAll("[,\\n]", " ").trim();
+        }
+
+        Comparator<String> comparator=new Comparator<String>(){
+            @Override
+            public int compare (String o1, String o2){
+                return o1.length()-o2.length();
+            }
+        };
+        Arrays.sort(sentences, comparator);
+        for (String sentens:sentences){
+            System.out.println(sentens);
+        }
+    }
+
+}
