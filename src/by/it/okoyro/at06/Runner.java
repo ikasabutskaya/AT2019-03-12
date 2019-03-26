8859b782da87f838b17933a99a475f2a8e1226ee
okoyro <oksana.koyro@gmail.com>
2019-03-26 11:17:57 +0300

    at06 homework done for Scalar & Vector

--- src/by/it/okoyro/at06/Runner.java
+++ src/by/it/okoyro/at06/Runner.java
@@ -8,10 +8,19 @@ public class Runner {
 		Var var2 = new Scalar(222); //объявление через абстрактный класс
 		System.out.println(var2);
 
-		Scalar var3 = new Scalar(333); //объявление через класс
+		Scalar var3 = new Scalar(333); //объявление через класс для получения экземпляра объекта Scalar, который передается в следующий конструктор
 		System.out.println(var3);
 
 		Var var4 = new Scalar(var3);
 		System.out.println(var4);
+
+		Var var5 = new Vector("{1.0, 2.0, 4.0}");
+		System.out.println(var5);
+
+		Var var6 = new Vector(new double[]{1.0, 2.0, 4.0});
+		System.out.println(var6);
+
+		Var var7 = new Vector(new Vector("{1.0, 2.0, 4.0}"));
+		System.out.println(var7);
 	}
 }
--- src/by/it/okoyro/at06/Vector.java
+++ src/by/it/okoyro/at06/Vector.java
@@ -10,16 +10,29 @@ public class Vector extends Var {
 	}
 
 	public Vector(String strVector) {
-		String[] s = strVector.replace("{","").replace("}","").split(",\\s*");
-		value=new double[s.length];
+		String[] s = strVector.replace("{", "")
+				.replace("}", "")
+				.split(",\\s*"); //сплитим строку и получаем массив строк
+		value = new double[s.length]; // создаем массив для double
 		for (int i = 0; i < s.length; i++) {
-			  value[i]=Double.parseDouble(String.valueOf(value));
+			value[i] = Double.parseDouble(s[i]); //перебираем массив строк и заполняем массив double
 		}
-
-				//{1, 2, 3, 4, 5}
 	}
 
 	public Vector(Vector vector) {
 		this.value = Arrays.copyOf(vector.value, vector.value.length);
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder("{");
+		for (int i = 0; i < value.length; i++) {
+			sb.append(value[i]);
+			if (i < value.length - 1) {
+				sb.append(", ");
+			}
 		}
+		sb.append("}");
+		return sb.toString();
+	}
 }
