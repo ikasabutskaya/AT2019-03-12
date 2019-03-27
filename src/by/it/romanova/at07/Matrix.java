df5cbb18c23eb350fe3788876e114443e22e9377
Liza <>
2019-03-27 22:43:39 +0300

    at07 C1 done

--- src/by/it/romanova/at07/Matrix.java
+++ src/by/it/romanova/at07/Matrix.java
@@ -98,7 +98,7 @@ class Matrix extends Var{
         for (int i = 0; i < arr.length; i++) {
             result = result.concat(Arrays.toString(arr[i])).replace('[','{').replace(']','}');
             if(i!=(arr.length-1))
-                result = result.concat(",");
+                result = result.concat(", ");
 
         }
         result = result.concat("}");
