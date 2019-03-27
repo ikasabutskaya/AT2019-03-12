5d040e8ba2b76fb4ceb0a958647f3264d8e61cab
Admin <udlv@tu.by>
2019-03-27 11:58:44 +0300

    +1

---
+++ src/by/it/udalyou/at07/Operation.java
@@ -0,0 +1,11 @@
+package by.it.udalyou.at07;
+
+abstract class Operation {
+    public abstract Var add(Var other);
+
+    public abstract Var sub(Var other);
+
+    public abstract Var mul(Var other);
+
+    public abstract Var div(Var other);
+}
