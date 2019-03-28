48d84fd9abfff2ed86b01abdb8d2309e52995de3
Tatsiana Tsyhanova <48747230+TatsianaTsyhanova@users.noreply.github.com>
2019-03-28 09:00:03 +0300

    Update Vector.java

--- src/by/it/tsyhanova/at07/Vector.java
+++ src/by/it/tsyhanova/at07/Vector.java
@@ -82,7 +82,7 @@ public class Vector extends Var {
         }
         return super.mul(other);
     }
-
+//vector is dividing by scalar or vector
     @Override
     public Var div(Var other) {
         if (other instanceof Scalar){
