67cb74a36c5bb1d08417fd3ac1223130759bcb39
TatsianaTsyhanova <tatsianatsyhanova@yahoo.com>
2019-03-28 12:14:52 +0300

    AT05 classwork level A

---
+++ src/by/it/tsyhanova/at08/Runner.java
@@ -0,0 +1,19 @@
+package by.it.tsyhanova.at08;
+
+import java.util.Scanner;
+
+public class Runner {
+    public static void main(String[] args) {
+
+        Parser parser=new Parser();
+        Printer printer=new Printer();
+
+        Scanner scanner=new Scanner(System.in);
+        for(;;){
+            String expression=scanner.nextLine();
+            if(expression.equals("end"))break;
+            Var var=parser.calc(expression);
+            printer.print(var);
+        }
+    }
+}
