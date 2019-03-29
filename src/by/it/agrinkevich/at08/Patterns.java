75a2149ce7a5d323fd70a7c4f94b071fb8e5cf01
agrinkevich <grinkevich.alina@gmail.com>
2019-03-29 22:16:37 +0300

    at08 - сomplete

--- src/by/it/agrinkevich/at08/Patterns.java
+++ src/by/it/agrinkevich/at08/Patterns.java
@@ -3,6 +3,6 @@ package by.it.agrinkevich.at08;
 public class Patterns {
     static final String SCALAR = "-?[0-9]+(\\.[0-9]+)?";
     static final String VECTOR = "\\{((-?[0-9]+(\\.[0-9]+)?),?)+}";
-    static final String MATRIX = "\\{\\{((-?[0-9]+(\\.[0-9]+)?),?)+},?)+}";
+    static final String MATRIX = "\\{(\\{((-?[0-9]+(\\.[0-9]+)?),?)+},?)+}";
     static final String OPERATION = "[-+*/=]";
 }
