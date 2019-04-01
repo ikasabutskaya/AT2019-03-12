69de8316ff75eb9ec2d01980ef28b8ac975aebc9
HPG4 <sa.simanovich@gmail.com>
2019-04-01 14:29:25 +0300

    1

--- src/by/it/Simanovich/at08/Patterns.java
+++ src/by/it/Simanovich/at08/Patterns.java
@@ -1,7 +1,7 @@
 package by.it.Simanovich.at08;
 
 public class Patterns {
-//    static final String Scalar = -?[0-9]+(\.?[0-9]+)?;
-//    static final String Vector = /{((-?[0-9]+(\.?[0-9]+)?),?)+};
-//    static final String Operation = "[-+*/=]"
+    static final String Scalar = "-?[0-9]+(\\.[0-9]+)?";
+    static final String Vector = "\\{((-?[0-9]+(\\.[0-9]+)?),?)+}";
+    static final String Operation = "[-+*/=]";
 }
