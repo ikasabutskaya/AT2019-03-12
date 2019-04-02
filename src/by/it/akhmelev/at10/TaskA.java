0503969037cd334d50e11f01382c4748f4c86e6b
DzianisMatveyenka <dzianis.matveyenka@mail.ru>
2019-04-02 12:20:21 +0300

    classwork at10

---
+++ src/by/it/akhmelev/at10/TaskA.java
@@ -0,0 +1,32 @@
+package by.it.akhmelev.at10;
+
+import java.util.HashMap;
+
+public class TaskA {
+    public static void main(String[] args) {
+        try {
+            if (Math.random() > 0.5)
+                new HashMap<String, String>(null);
+            else
+                Integer.parseInt("привет");
+        } catch (NullPointerException | NumberFormatException e) {
+            printInfoException(e);
+        }
+    }
+
+    private static void printInfoException(RuntimeException e) {
+        String name = e.getClass().getName();
+        StackTraceElement[] stackTrace = e.getStackTrace();
+        for (StackTraceElement element : stackTrace) {
+            String myClassName = element.getClassName();
+            int lineNumber = element.getLineNumber();
+            if (myClassName.equals(TaskA.class.getName())) {
+                System.out.printf(
+                        " name: %s\n" +
+                        "class: %s\n" +
+                        " line: %d\n",
+                        name,myClassName,lineNumber);
+            }
+        }
+    }
+}
