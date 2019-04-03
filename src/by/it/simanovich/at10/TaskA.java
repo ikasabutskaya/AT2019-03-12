075f3ab3ca0a8139b88197863dca32a1cde36aac
HPG4 <sa.simanovich@gmail.com>
2019-04-03 18:28:40 +0300

    1

---
+++ src/by/it/simanovich/at10/TaskA.java
@@ -0,0 +1,11 @@
+package by.it.simanovich.at10;
+
+import java.util.HashMap;
+
+public class TaskA {
+
+    if (Math.random()>0.5)
+            new HashMap<String, String>(null);
+else
+        Integer.parseInt("привет");
+}
