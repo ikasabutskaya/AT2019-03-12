1ea7eed7ad6abe1373a0b8248fdd3766d7ad6f18
okoyro <oksana.koyro@gmail.com>
2019-04-07 16:40:38 +0300

    at12 homework (A level) done

--- src/by/it/okoyro/at12/TaskA.java
+++ src/by/it/okoyro/at12/TaskA.java
@@ -1,6 +1,8 @@
 package by.it.okoyro.at12;
 
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -8,17 +10,41 @@ import java.io.PrintWriter;
 public class TaskA {
 	public static void main(String[] args) {
 
-		String filename = System.getProperty("user.dir") + "/src/by/it/okoyro/at12/matrix.txt";  // TO DO expand path
+		String filename = System.getProperty("user.dir") + "/src/by/it/okoyro/at12/matrix.txt";
 
 		int[][] array = new int[6][4];
 
-		while (!generate(array)) ; //метод с true/false return заполняет матрицу случайными числами
+		while (!generate(array)) ; //метод с true/false return-ом заполняет матрицу случайными числами
 
+		printMatrixToFile(filename, array);
+		readMatrixFromFile(filename);
+	}
+
+	private static boolean generate(int[][] array) {
+		boolean minus15 = false;
+		boolean plus15 = false;
+
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[j].length; j++) { //??? почему указываем [j] и не указваем [i] ???
+				array[i][j] = (int) (Math.random() * 31) - 15;
+				if (!minus15 && array[i][j] == -15) {
+					minus15 = true;
+				}
+				if (!plus15 && array[i][j] == 15) {
+					plus15 = true;
+				}
+			}
+		}
+		return minus15 && plus15; // boolean method why return???
+	}
+
+	private static void printMatrixToFile(String filename, int[][] array) {
 		try (PrintWriter pw = new PrintWriter(new FileWriter(filename))) {
 			for (int[] row : array) {
 				for (int element : row) {
-					System.out.printf("%3d ", element); // incorrect
+					pw.printf("%3d ", element);
 				}
+				pw.println();
 			}
 		}
 		catch (IOException e) {
@@ -26,21 +52,18 @@ public class TaskA {
 		}
 	}
 
-	private static boolean generate(int[][] array) {
-		boolean minus15 = false;
-		boolean plus15 = false;
-
-		for (int i = 0; i < array[i].length; i++) {
-			for (int j = 0; j < array[j].length; j++) { //??? почему указываем [j] ???
-				array[i][j] = (int) (Math.random() * 31) - 15;
-				if (!minus15 && array[i][j] == -15) {
-					minus15 = true;
-				}
-				if (!plus15 && array[i][j] == -15) {
-					plus15 = true;
-				}
+	private static void readMatrixFromFile(String filename) {
+		try (BufferedReader br = new BufferedReader(
+				new FileReader(filename)
+		)) {
+			while (true) {
+				String line = br.readLine();
+				if (line==null) break;
+				System.out.println(line);
 			}
 		}
-		return minus15 && plus15;
+		catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
