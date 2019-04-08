32fbe02782244bbda5b4e79971e09fa0fa1b0b6c
Admin <udlv@tu.by>
2019-04-08 20:26:17 +0300

    at2-all,at3-all,at6-all,at12-A

--- src/by/it/udalyou/at07/Runner.java
+++ src/by/it/udalyou/at07/Runner.java
@@ -9,7 +9,7 @@ private static void print(Var var) {System.out.println(var);}
 public static void main(String[] args) throws CalkException, CalcException {
 Var s = new Scalar(3.0);
 Var v = new Vector(new double[]{1,2,3}); // закомментируйте вектор и/или
-//Var m = new Matrix("{{1,2,3},{4,5,6},{7,8,9}}");// матрицу, если вы их не реализовали
+Matrix m = new Matrix("{{1,2,3},{4,5,6},{7,8,9}}");// матрицу, если вы их не реализовали
 /* Уровень сложности A (калькулятор) */
 print(s.add(s)); //выведет в консоль 6.0
 print(s.sub(s)); //выведет в консоль 0.0
