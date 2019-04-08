c4459310cd65560a36766b20151cdd740c0acedb
Liza <>
2019-04-09 01:05:46 +0300

    at11 A-C complete

--- src/by/it/romanova/at11/TaskB.java
+++ src/by/it/romanova/at11/TaskB.java
@@ -2,6 +2,7 @@ package by.it.romanova.at11;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
@@ -10,29 +11,72 @@ public class TaskB {
 
     public static void main(String[] args) {
 
-        String path = System.getProperty("user.dir") + "/src/by/it/romanova/at11/";
+        String sourcePath = System.getProperty("user.dir") + "/src/by/it/romanova/at11/TaskB.txt";
+        String outPath = System.getProperty("user.dir") + "/src/by/it/romanova/at11/resultTaskB.txt";
 
-        try (Reader fr = new FileReader(path+"text.txt");
-            BufferedReader br = new BufferedReader(fr)
-            ){
+        ArrayList<String> arr = extractArrayOfLines(sourcePath);
 
-            ArrayList<String> arr = new ArrayList<>();
-            while (br.read()>0){
-                arr.add(br.readLine());
-            }
-//
-//            Pattern symbols = Pattern.compile("[^(\\w\\s)]");
-//            Matcher m = symbols.
-//
-//            for (String s : arr) {
-//                s.m
-//            }
+        int wordsCount = getWordsCount(arr);
+        int punct = getPunct(arr);
+
+        String result = "words=" + wordsCount + ", " + "punctuation marks=" + punct;
+
+        System.out.println(result);
+
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outPath))) {
+            bufferedWriter.write(result);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        //System.out.println(arr);
 
+    }
+
+    private static int getPunct(ArrayList<String> arr) {
+        int punct = 0;
+        for (String line : arr) {
+            Pattern punctiationPattern = Pattern.compile("[^а-яА-ЯёЁ\\s]");
+            Matcher punctuationMatcher = punctiationPattern.matcher(line);
+
+            while (punctuationMatcher.find()) {
+                punct++;
+            }
+
+        }
+        return punct;
+    }
+
+    private static int getWordsCount(ArrayList<String> arr) {
+        int wordsCount = 0;
+        for (String line : arr) {
+            Pattern wordsPattern = Pattern.compile("[а-яА-ЯёЁ]+");
+            Matcher wordsMatcher = wordsPattern.matcher(line);
+
+            while (wordsMatcher.find()) {
+                wordsCount++;
+            }
+
+        }
+        return wordsCount;
+    }
+
+    private static ArrayList<String> extractArrayOfLines(String path) {
+        ArrayList<String> arr = new ArrayList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))){
+            String line;
+            while ((line = bufferedReader.readLine())!=null){
+                if(line.contains("...")){
+                    line = line.replace("...", ".");
+                }
+                arr.add(line);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return arr;
     }
 
 }
