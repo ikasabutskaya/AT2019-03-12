7e62f3b1cd0935236454066bea8ee90b03b1d21e
TatsianaTsyhanova <tatsianatsyhanova@yahoo.com>
2019-04-10 15:15:00 +0300

    at13 tests for Scalar

--- src/by/it/tsyhanova/calc/ParserTest.java
+++ src/by/it/tsyhanova/calc/ParserTest.java
@@ -16,6 +16,33 @@ public class ParserTest {
     }
 
     @Test
+    public void calcScalarSubTest() throws CalcException {
+        Parser p=new Parser();
+        Var actual = p.calc("4-2");
+        double actualValue=Double.parseDouble(actual.toString());
+        double expectedValue=2.0;
+        assertEquals("Вычитание работает неверно!",expectedValue,actualValue,1e-10);
+    }
+
+    @Test
+    public void calcScalarMultTest() throws CalcException {
+        Parser p=new Parser();
+        Var actual = p.calc("4*2");
+        double actualValue=Double.parseDouble(actual.toString());
+        double expectedValue=8.0;
+        assertEquals("Умножение работает неверно!",expectedValue,actualValue,1e-10);
+    }
+
+    @Test
+    public void calcScalarDivTest() throws CalcException {
+        Parser p=new Parser();
+        Var actual = p.calc("4/2");
+        double actualValue=Double.parseDouble(actual.toString());
+        double expectedValue=2.0;
+        assertEquals("Деление работает неверно!",expectedValue,actualValue,1e-10);
+    }
+
+    @Test
     public void calcInstanceOfScalarTest() throws CalcException {
         //fail();
         Parser p=new Parser();
