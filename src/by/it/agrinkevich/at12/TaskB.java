f3b46e6f9e2836634bad47c057e3bdb13e58e391
agrinkevich <grinkevich.alina@gmail.com>
2019-04-11 01:11:25 +0300

    at12 - TaskB

--- src/by/it/agrinkevich/at12/TaskB.java
+++ src/by/it/agrinkevich/at12/TaskB.java
@@ -1,9 +1,10 @@
 package by.it.agrinkevich.at12;
+
 import java.io.*;
 
 public class TaskB {
     public static void main(String[] args) {
-        String filename = System.getProperty("user.dir")+
+        String filename = System.getProperty("user.dir") +
                 "/src/by/it/agrinkevich/at12/TaskB.java"; // это строка
         StringBuilder sb = new StringBuilder();
         readCodeLines(filename, sb); // вызов функции
@@ -11,65 +12,79 @@ public class TaskB {
     }
 
     private static void saveCode(String filename, StringBuilder sb) {
-        try(FileWriter fw=new FileWriter(System.getProperty("user.dir")+"/src/by/it/agrinkevich/at12/TaskB.txt");
-            PrintWriter pw=new PrintWriter(fw))
-        {
+        try (FileWriter fw = new FileWriter(System.getProperty("user.dir") + "/src/by/it/agrinkevich/at12/TaskB.txt");
+             PrintWriter pw = new PrintWriter(fw)) {
             pw.print(sb);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    /**
-     *
-     * @param filename путь к текущему джава файлу
-     */
+   /**
+    * @param filename путь к текущему джава файлу
+    **/
     private static void readCodeLines(String filename, StringBuilder sb) {
         /*
         первый и
         второй
         комментарии
         */
-        try(BufferedReader fr = new BufferedReader(new FileReader(filename)))
-        {
+        try (BufferedReader fr = new BufferedReader(new FileReader(filename))) {
             boolean isMultipleComment = false;
-            while (true){
+            while (true) {
                 String line = fr.readLine();
                 if (line == null) break;
-                int indexOfComment1 = line.indexOf("//");
-                int indexOfComment2 = line.indexOf("/*");
-                int indexOfComment3 = line.indexOf("*/");
+                int indexOfComment1;
+                int indexOfComment2;
+                int indexOfComment3;
 
-                if (indexOfComment2 != -1) {
-                    char previousChar = line.charAt(indexOfComment2 - 1);
-                    if(previousChar != '"'){
-                        isMultipleComment = true;
-                        continue;
+
+                int slashIndex = line.lastIndexOf("/");
+                if (slashIndex == -1) {
+                    indexOfComment3 = -1;
+                    indexOfComment2 = -1;
+                    indexOfComment1 = -1;
+                } else {
+                    char beforeSlashChar = line.charAt(slashIndex - 1);
+                    if (beforeSlashChar == '*') {
+                        indexOfComment3 = slashIndex - 1;
+                        indexOfComment2 = -1;
+                        indexOfComment1 = -1;
+                    } else if(beforeSlashChar == '/'){
+                        indexOfComment3 = -1;
+                        indexOfComment2 = -1;
+                        indexOfComment1 = slashIndex-1;
+                    } else {
+                        indexOfComment1 = -1;
+                        indexOfComment3 = -1;
+                        char afterSlashChar = line.charAt(slashIndex + 1);
+                        if (afterSlashChar == '*') {
+                            indexOfComment2 = slashIndex;
+                        } else {
+                            indexOfComment2 = -1;
+                        }
                     }
                 }
 
+
+                if (indexOfComment2 != -1) {
+                    isMultipleComment = true;
+                    sb.append(line.substring(0,indexOfComment2)).append("\n");
+                    continue;
+                }
+
                 if (indexOfComment3 != -1) {
-                    char previousChar = line.charAt(indexOfComment3 - 1);
-                    if(previousChar != '"'){
-                        isMultipleComment = false;
-                        sb.append("\n");
-                        continue;
-                    }
+                    isMultipleComment = false;
+                    continue;
                 }
 
                 if (isMultipleComment)
                     continue;
 
                 if (indexOfComment1 == -1) {
-                        sb.append(line).append("\n");
-                }
-                else {
-                    char previousChar = line.charAt(indexOfComment1 - 1);
-                    if(previousChar == '"'){
-                        sb.append(line).append("\n");
-                    }else {
-                        sb.append(line, 0, indexOfComment1).append("\n");
-                    }
+                    sb.append(line).append("\n");
+                } else {
+                    sb.append(line.substring(0,indexOfComment1)).append("\n");
                 }
             }
             /*
@@ -78,7 +93,7 @@ public class TaskB {
             семь
             */
             System.out.println(sb);
-            } catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
