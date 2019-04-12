99d956b1672e3808cc2b9256a57339a953aaf2cd
agrinkevich <grinkevich.alina@gmail.com>
2019-04-12 20:39:05 +0300

    test commit

---
+++ src/by/it/agrinkevich/calc/at14/ScalarSumTest.java
@@ -0,0 +1,11 @@
+package by.it.agrinkevich.calc.at14;
+
+public class ScalarSumTest {
+
+
+
+   /* public void sum(){
+        Parser parser=new ParserTest();
+        parser.calc("2+2");
+    }*/
+}
