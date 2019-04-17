6d8246a873e4d01c0590ffc1d23fdba5fe105173
okoyro <oksana.koyro@gmail.com>
2019-04-17 22:36:44 +0300

    at16 homework STAX parser

--- src/by/it/okoyro/at16_tax_stax_xslt/STAX.java
+++ src/by/it/okoyro/at16_tax_stax_xslt/STAX.java
@@ -18,17 +18,35 @@ public class STAX {
 		String filename = "/Users/oksanakoyro/work/AT2019-03-12/src/by/it/okoyro/at15/sites.xml";
 		Reader reader = new FileReader(filename);
 		XMLStreamReader xmlStreamReader = factory.createXMLStreamReader(reader);
+
 		while (xmlStreamReader.hasNext()) {
-			int type = xmlStreamReader.next();
-			switch (type) {
+			int nodeType = xmlStreamReader.next();
+			switch (nodeType) {
 				case XMLStreamConstants.START_ELEMENT:
-					System.out.println("start");
+
+					StringBuilder at = new StringBuilder();
+					int length = xmlStreamReader.getAttributeCount();
+					for (int i = 0; i < length; i++) {
+						at.append(" ").append(xmlStreamReader.getAttributeLocalName(i))
+								.append("=").append(xmlStreamReader.getAttributeValue(i));
+					}
+					System.out.println(tab + "<" + xmlStreamReader.getLocalName() + at + ">");
+					tab = tab.concat("\t"); // добавить табулятор
+
 					break;
 				case XMLStreamConstants.CHARACTERS:
-					System.out.println("text");
+
+					text.append(xmlStreamReader.getText());
+
 					break;
 				case XMLStreamConstants.END_ELEMENT:
-					System.out.println("end");
+					tab = tab.substring(1);  // удалить табулятор
+					String out = text.toString().trim();
+					if (!out.isEmpty()) {
+						System.out.println(tab + out);
+						text.setLength(0); // чистим StringBuilder
+					}
+					System.out.println(tab + "</" + xmlStreamReader.getLocalName() + ">");
 					break;
 				default:
 			}
