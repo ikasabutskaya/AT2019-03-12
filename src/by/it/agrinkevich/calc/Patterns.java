41c9b4046ce91a89284869ee916ad8b290998cef
agrinkevich <grinkevich.alina@gmail.com>
2019-04-20 11:39:53 +0300

    test commit

--- src/by/it/agrinkevich/calc/Patterns.java
+++ src/by/it/agrinkevich/calc/Patterns.java
@@ -4,6 +4,6 @@ public class Patterns {
     static final String SCALAR="-?[0-9]+(\\.[0-9]+)?";
     static final String VECTOR="\\{((-?[0-9]+(\\.[0-9]+)?),?)+}";
     static final String MATRIX="\\{(\\{((-?[0-9]+(\\.[0-9]+)?),?)+},?)+}";
-    static final String OPERATION="[-+*/=]";
+    static final String OPERATION="(?<=[^{,=+*/-])([-+*/=])";
     static final String OPERATION_REGEXP="(.*)[-+*/=](.*)";
 }
--- src/by/it/agrinkevich/calc/at14/MatrixMulTest.java
+++ src/by/it/agrinkevich/calc/at14/MatrixMulTest.java
@@ -24,7 +24,7 @@ public class MatrixMulTest {
                 {"{{2, 3, 4}, {2, 3, 4}, {2, 3, 4}} * 2", "{{4.0, 6.0, 8.0}, {4.0, 6.0, 8.0}, {4.0, 6.0, 8.0}}"},
                 {"{{1, 2},{1, 2}} * {1, 2}", "{5.0, 5.0}"},
                 {"{{-4,3,1},{0,7,2},{-8,2,5}}*{{5,1,-3},{-2,4,8},{11,3,-9}}", "{{-15.0, 11.0, 27.0}, {8.0, 34.0, 38.0}, {11.0, 15.0, -5.0}}"},
-                {"{{1.4, 2.2}, {1.5, 2.3}} * {3.11, -6.4}", "{-9.726, -10.055}"}
+                {"{{1.4,2.2}, {1.5,2.3}} * {3.11,-6.4}", "{-9.726, -10.055}"}
         };
         return Arrays.asList(str);
     }
@@ -38,6 +38,12 @@ public class MatrixMulTest {
     @Test
     public void mul() throws CalcException {
         Var actual = parser.calc(expression);
+        //double actualDouble = Double.parseDouble(actual);
+        //double expectedDouble = Double.parseDouble(result);
+        //Assert.assertEquals(expectedDouble, actualDouble, 1e-10);
+        // так как actualDouble и expectedDuble - это вектора, то мы должны получить массив даблов (getValue())
+        // и сравнить друг с другом нижним методом:
+        //Assert.assertArrayEquals();
         Assert.assertThat(result, is(actual.toString()));
         System.out.println("passed: "+expression+"="+result);
     }
