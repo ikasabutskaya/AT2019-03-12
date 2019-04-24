d0dcc5c0f56df21aeb9a903e71cdc64885212d67
Admin <udlv@tu.by>
2019-04-24 19:46:29 +0300

    Задание17

--- src/by/it/udalyou/at14evlCalk/Patterns.java
+++ src/by/it/udalyou/at14evlCalk/Patterns.java
@@ -1,7 +1,7 @@
 package by.it.udalyou.at14evlCalk;
 
 public class Patterns {
-    static final String SCALAR="-?[0-9]+(\\.[0-9]+)?";
+    static final String SCALAR="[-+]?[0-9]+(\\.[0-9]+)?";
     static final String VECTOR="\\{((-?[0-9]+(\\.[0-9]+)?),?)+}";
     static final String MATRIX="\\{(\\{((-?[0-9]+(\\.[0-9]+)?),?)+},?)+}";
     static final String OPERATION="(?<=[^{,=+*/-])([-+*/=])";
