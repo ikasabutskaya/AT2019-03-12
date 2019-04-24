32781443a662ce6650e1c910085f7ffdbb8bcde2
Stanishevski <oleg.stanishevski@gmail.com>
2019-04-24 21:11:36 +0300

    ok

--- src/by/it/stanishevski/at10/Operation.java
+++ src/by/it/stanishevski/at10/Operation.java
@@ -1,8 +1,8 @@
 package by.it.stanishevski.at10;
 
 interface Operation {
-Var add (Var other) throws CalcExeption;
-Var sub (Var other) throws CalcExeption;
-Var mul (Var other) throws CalcExeption;
-Var div (Var other) throws CalcExeption;
+Var add(Var other) throws CalcExeption;
+Var sub(Var other) throws CalcExeption;
+Var mul(Var other) throws CalcExeption;
+Var div(Var other) throws CalcExeption;
 }
