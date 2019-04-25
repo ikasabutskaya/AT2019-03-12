3e56e820e2e870c6587dd3566aa6a065a859c083
Alexander Khmelov <akhmelev@gmail.com>
2019-04-25 20:40:12 +0300

    at20 fix

--- src/by/it/akhmelev/at20/create.sql
+++ src/by/it/akhmelev/at20/create.sql
@@ -2,7 +2,7 @@
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
 -- Schema akhmelev
@@ -20,11 +20,11 @@ USE `akhmelev` ;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `akhmelev`.`user` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(45) NULL,
-  `email` VARCHAR(45) NULL,
-  `password` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+  `username` VARCHAR(16) NOT NULL,
+  `email` VARCHAR(255) NULL,
+  `password` VARCHAR(32) NOT NULL,
+  `create_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`));
 
 
 -- -----------------------------------------------------
@@ -32,9 +32,9 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `akhmelev`.`category` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+  `name` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE);
 
 
 -- -----------------------------------------------------
@@ -47,8 +47,8 @@ CREATE TABLE IF NOT EXISTS `akhmelev`.`variable` (
   `user_id` INT NOT NULL,
   `category_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_variable_user_idx` (`user_id` ASC),
-  INDEX `fk_variable_category1_idx` (`category_id` ASC),
+  INDEX `fk_variable_user_idx` (`user_id` ASC) VISIBLE,
+  INDEX `fk_variable_category1_idx` (`category_id` ASC) VISIBLE,
   CONSTRAINT `fk_variable_user`
     FOREIGN KEY (`user_id`)
     REFERENCES `akhmelev`.`user` (`id`)
@@ -71,9 +71,9 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `akhmelev`;
-INSERT INTO `akhmelev`.`user` (`id`, `username`, `email`, `password`) VALUES (DEFAULT, 'default', 'def@mail.tu', 'qwerty');
-INSERT INTO `akhmelev`.`user` (`id`, `username`, `email`, `password`) VALUES (DEFAULT, 'user', 'user@mail.ru', 'asdfgh');
-INSERT INTO `akhmelev`.`user` (`id`, `username`, `email`, `password`) VALUES (DEFAULT, 'admin', 'admin@google.com', 'zxcvbn');
+INSERT INTO `akhmelev`.`user` (`id`, `username`, `email`, `password`, `create_time`) VALUES (1, 'default', 'def@mail.ru', 'qwerty', DEFAULT);
+INSERT INTO `akhmelev`.`user` (`id`, `username`, `email`, `password`, `create_time`) VALUES (2, 'user', 'user@mail.ru', 'asdfgh', DEFAULT);
+INSERT INTO `akhmelev`.`user` (`id`, `username`, `email`, `password`, `create_time`) VALUES (3, 'admin', 'admin@google.com', 'zxcvbn', DEFAULT);
 
 COMMIT;
 
@@ -83,9 +83,9 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `akhmelev`;
-INSERT INTO `akhmelev`.`category` (`id`, `name`) VALUES (DEFAULT, 'SCALAR');
-INSERT INTO `akhmelev`.`category` (`id`, `name`) VALUES (DEFAULT, 'VECTOR');
-INSERT INTO `akhmelev`.`category` (`id`, `name`) VALUES (DEFAULT, 'MATRIX');
+INSERT INTO `akhmelev`.`category` (`id`, `name`) VALUES (1, 'SCALAR');
+INSERT INTO `akhmelev`.`category` (`id`, `name`) VALUES (2, 'VECTOR');
+INSERT INTO `akhmelev`.`category` (`id`, `name`) VALUES (3, 'MATRIX');
 
 COMMIT;
 
