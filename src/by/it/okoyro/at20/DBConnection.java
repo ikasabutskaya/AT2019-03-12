d11a19624005af04ce4c291ed9201baf604b40ac
okoyro <oksana.koyro@gmail.com>
2019-05-08 12:44:29 +0300

    at_20 DB connection

--- src/by/it/okoyro/at20/DBConnection.java
+++ src/by/it/okoyro/at20/DBConnection.java
@@ -6,11 +6,15 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class DBRuner {
+public class DBConnection {
 	public static void main(String[] args) throws SQLException {
 			try (
 					Connection connection = DriverManager.getConnection(
-							"jdbc:mysql://localhost:3306/okoyro?useSSL=false&connectTimeout=0&autoReconnect=true&useUnicode=true&characterEncoding=UTF-8",
+							"jdbc:mysql://localhost:3306/okoyro?useSSL=false" +
+							"&connectTimeout=0" +
+							"&autoReconnect=true" +
+							"&useUnicode=true" +
+							"&characterEncoding=UTF-8",
 //							+
 //							"unicode = true&" +
 //							"useJDBCCompliantTimezoneShift = true&" +
