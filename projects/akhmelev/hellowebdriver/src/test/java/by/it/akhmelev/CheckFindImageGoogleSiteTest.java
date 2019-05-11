32024fa3defc8ed616c9c4e903da4b2f2707ab52
Ulyana <okini-san25@mail.ru>
2019-05-11 12:31:51 +0300

    at24 classwork TaskA (started)

---
+++ projects/akhmelev/hellowebdriver/src/test/java/by/it/akhmelev/CheckFindImageGoogleSiteTest.java
@@ -0,0 +1,59 @@
+package by.it.akhmelev;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class CheckFindImageGoogleSiteTest {
+
+    private WebDriver driver;
+
+    private static WebElement waitAndGetWebElement(WebDriver driver, By queryLocator) {
+        return (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.presenceOfElementLocated(queryLocator));
+    }
+
+    @Before
+    public void setUpBrowser() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void taskA() throws Exception {
+        driver.get("https://www.google.com/");
+        By byQueryInput = By.xpath("//input[@class='gLFyf gsfi']");
+        WebElement queryInput = waitAndGetWebElement(driver, byQueryInput);
+        queryInput.sendKeys("seleniumhq\n");
+
+        By byLinkImagesSearch = By.xpath("//*[@class='hdtb-mitem hdtb-imb'][1]/a");
+        WebElement linkImagesSearch = waitAndGetWebElement(driver, byLinkImagesSearch);
+        linkImagesSearch.click();
+
+        By byLinkResultFirstImage = By.xpath("//*[@class='rg_bx rg_di rg_el ivg-i'][@data-ri='0']");
+        WebElement linkResultFirstImage = waitAndGetWebElement(driver, byLinkResultFirstImage);
+        linkResultFirstImage.click();
+
+        By byLinkResultSite = By.xpath("//a[@class='irc_vpl i3599 irc_lth'][@tabindex='0']");
+        WebElement linkResultSite = waitAndGetWebElement(driver, byLinkResultSite);
+        linkResultSite.click();
+
+        //TODO add end
+
+        Assert.assertEquals("1", "1");
+    }
+
+    @After
+    public void tearDownBrowser() {
+        driver.quit();
+    }
+
+
+}
