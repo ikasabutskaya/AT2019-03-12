e411a877d91ad8426d61026f99cc2df775ac07f2
Admin <udlv@tu.by>
2019-05-12 14:37:38 +0300

    23

--- projects/udalyou/hellowebdriver/src/test/java/by/i/udalyou/CheckGoogleSiteTest.java
+++ projects/udalyou/hellowebdriver/src/test/java/by/i/udalyou/CheckGoogleSiteTest.java
@@ -1,5 +1,6 @@
 package by.i.udalyou;
 
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -7,24 +8,30 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class helloselenium {
-    public static void main(String[] args) throws InterruptedException {
+public class CheckGoogleSiteTest {
+    @Test
+    public void userTryFindHelloInGoogle () throws Exception{
+
         WebDriver driver=new ChromeDriver();
-        driver.get("http://googl.com");
+       try
+       { driver.get("http://googl.com");
         By queryLocator=By.xpath
                 ("//input[@class='gLFyf gsfi']");
 
         (new WebDriverWait(driver, 10))
                 .until(ExpectedConditions.presenceOfElementLocated(queryLocator));
-    WebElement queryInput=waitAndGetWebElement(driver,queryLocator);
-    queryInput.sendKeys("привет \n");
-       waitAndGetWebElement(driver,By.xpath(
-               "//span[@class=\"csb ch\"]"));
-        driver.quit();
+        WebElement queryInput=waitAndGetWebElement(driver,queryLocator);
+        queryInput.sendKeys("привет \n");
+        waitAndGetWebElement(driver,By.xpath(
+                "//span[@class=\"csb ch\"]"));
+    }
+       finally {
+           driver.quit();
+       }
     }
     private static WebElement waitAndGetWebElement(WebDriver driver, By queryLocator) {
         (new WebDriverWait(driver, 5))
-                .until(ExpectedConditions.presenceOfElementLocated(queryLocator));
+            .until(ExpectedConditions.presenceOfElementLocated(queryLocator));
         return driver.findElement(queryLocator);
     }
 }
