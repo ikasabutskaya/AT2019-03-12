1f230a4802c5b956ca85542e4def659cab7fd178
agrinkevich <grinkevich.alina@gmail.com>
2019-05-13 23:47:53 +0300

    a24 - a, b partially

--- projects/agrinkevich/hellowebdriver/src/test/java/by/it/agrinkevich/CheckFindImageGoogleSiteTest.java
+++ projects/agrinkevich/hellowebdriver/src/test/java/by/it/agrinkevich/CheckFindImageGoogleSiteTest.java
@@ -7,8 +7,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import static junit.framework.TestCase.assertTrue;
 
 public class CheckFindImageGoogleSiteTest {
 
@@ -44,8 +51,64 @@ public class CheckFindImageGoogleSiteTest {
         WebElement linkResultSite = waitAndGetWebElement(driver, byLinkResultSite);
         linkResultSite.click();
 
-        Thread.sleep(2000);
+        //open a new tab and check url at the last tab
+        Set<String> handles = driver.getWindowHandles();
+        List<String> handlesList = new ArrayList<>(handles);
+        String newTab = handlesList.get(handlesList.size() - 1);
 
+        // switch to new tab
+        driver.switchTo().window(newTab);
+
+        String url = driver.getCurrentUrl();
+        assertTrue("not complete find", url.startsWith("https://www.seleniumhq.org"));
+    }
+
+    @Test
+    public void TaskB() throws Exception{
+        driver.get("https://www.google.com/");
+        By byQueryInput = By.xpath("//input[@class='gLFyf gsfi']");
+        WebElement queryInput = waitAndGetWebElement(driver, byQueryInput);
+        queryInput.sendKeys("seleniumhq\n");
+
+        By byLinkImagesSearch = By.xpath("//*[@class='hdtb-mitem hdtb-imb'][1]/a");
+        WebElement linkImagesSearch = waitAndGetWebElement(driver, byLinkImagesSearch);
+        linkImagesSearch.click();
+
+        By byLinkResultFirstImage = By.xpath("//*[@class='rg_bx rg_di rg_el ivg-i'][@data-ri='0']");
+        WebElement linkResultFirstImage = waitAndGetWebElement(driver, byLinkResultFirstImage);
+        linkResultFirstImage.click();
+
+        By byLinkResultSite = By.xpath("//*[@class='irc_vpl i3599 irc_lth'][@tabindex='0']");
+        WebElement linkResultSite = waitAndGetWebElement(driver, byLinkResultSite);
+        linkResultSite.click();
+
+        Set<String> handles = driver.getWindowHandles();
+        List<String> handlesList = new ArrayList<>(handles);
+        String newTab = handlesList.get(handlesList.size() - 1);
+
+        driver.switchTo().window(newTab);
+
+        String url = driver.getCurrentUrl();
+        assertTrue("not complete find", url.startsWith("https://www.seleniumhq.org"));
+
+        assertTrue(driver.getTitle().contains("Selenium - Web Browser Automation"));
+
+        newTab = handlesList.get(0);
+        driver.switchTo().window(newTab);
+
+        By byLinkSearch = By.xpath("//*[@class='RNNXgb']");
+        //*[@class='gLFyf gsfi'])
+        WebElement linkSearch = waitAndGetWebElement(driver, byLinkSearch);
+        Actions builder = new Actions(driver);
+        builder.dragAndDrop(linkResultFirstImage, linkSearch).build().perform();
+        //builder.clickAndHold(linkResultFirstImage).moveToElement(linkSearch).release().build().perform();
+
+
+        /*By byLinkFirstResultSite = By.xpath("//*[@id=\"rso\"]/div[1]/div/div[1]/div/div/div[1]/a[1]/h3");
+        WebElement linkFirstResultSite = waitAndGetWebElement(driver, byLinkFirstResultSite);
+        linkFirstResultSite.click();*/
+
+        Thread.sleep(5000);
     }
 
     @After
