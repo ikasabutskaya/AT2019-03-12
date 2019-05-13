79bb0484e1a29db0c804e995a348c9e12ea116aa
Admin <udlv@tu.by>
2019-05-13 22:17:57 +0300

    at23-1

--- projects/udalyou/hellowebdriver/src/test/java/by/it/udalyou/CheckFindImageGoogleSiteTest.java
+++ projects/udalyou/hellowebdriver/src/test/java/by/it/udalyou/CheckFindImageGoogleSiteTest.java
@@ -33,9 +33,12 @@ public void TaskA  ()throws Exception{
         seachedlement.sendKeys("https://www.seleniumhq.org/\n");
         Thread.sleep(5000);
 
-        By seach2 = By.xpath("//*[@id=\"hdtb - msb - vis\"]/div[3]/a");
+        /*By seach2 = By.xpath("//*[@id=\"hdtb - msb - vis\"]/div[3]/a");
         WebElement seachedlement2 = waitAndGetWebElement(driver, seach2);
-        seachedlement2.click();
+        seachedlement2.click();*/
+        By byLinkImagesSearch = By.xpath("//a[@class='q qs'][contains(@href,'tbm=isch')]");
+        WebElement linkImagesSearch = waitAndGetWebElement(driver, byLinkImagesSearch);
+        linkImagesSearch.click();
 
         Assert.assertEquals("1","1");
 
@@ -46,3 +49,58 @@ public void TaskA  ()throws Exception{
        driver.quit();
     }
 }
+
+   /* private WebDriver driver;
+
+    private static WebElement waitAndGetWebElement(WebDriver driver, By queryLocator) {
+        return (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.presenceOfElementLocated(queryLocator));
+    }
+
+    @Before
+    public void setUpBrowser() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void taskA() throws Exception {
+        driver.get("https://www.google.com");
+        // Engish page
+        // driver.get("https://www.google.com/webhp?hl=en");
+        By byQueryInput = By.xpath("//input[@class='gLFyf gsfi']");
+        WebElement queryInput = waitAndGetWebElement(driver, byQueryInput);
+        queryInput.sendKeys("seleniumhq\n");
+
+        By byLinkImagesSearch = By.xpath("//a[@class='q qs'][contains(@href,'tbm=isch')]");
+        WebElement linkImagesSearch = waitAndGetWebElement(driver, byLinkImagesSearch);
+        linkImagesSearch.click();
+
+        By byLinkResultFirstImage = By.xpath("//*[@class='rg_bx rg_di rg_el ivg-i'][@data-ri='0']");
+        WebElement linkResultFirstImage = waitAndGetWebElement(driver, byLinkResultFirstImage);
+        linkResultFirstImage.click();
+
+        By byLinkResultSite = By.xpath("//a[@class='irc_vpl i3599 irc_lth'][@tabindex=0]");
+        WebElement linkResultSite = waitAndGetWebElement(driver, byLinkResultSite);
+
+        //wait rendering (но лучше проверить это явно)
+        Thread.sleep(333);
+        linkResultSite.click();
+
+        //open a new tab and check url at the last tab
+        Set<String> handles = driver.getWindowHandles();
+        List<String> handlesList = new ArrayList<>(handles);
+        String newTab = handlesList.get(handlesList.size() - 1);
+
+        // switch to new tab
+        driver.switchTo().window(newTab);
+
+        String url = driver.getCurrentUrl();
+        assertTrue("not complete find", url.startsWith("https://www.seleniumhq.org"));
+    }
+
+    @After
+    public void tearDownBrowser() {
+        driver.quit();
+    }*/
+
