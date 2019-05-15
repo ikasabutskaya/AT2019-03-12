a9ab4feecf41bc41dcaca7d8ded164040fd4707f
Vitali Shchur <vitali.shchur@gmail.com>
2019-05-16 00:09:49 +0300

    at19 A, B, C

---
+++ src/by/it/vshchur/at22/InterfaceDao.java
@@ -0,0 +1,16 @@
+package by.it.vshchur.at22;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface InterfaceDao<TYPE> {
+    TYPE read(long id) throws SQLException;
+
+    boolean create(TYPE bean) throws SQLException;
+
+    boolean update(TYPE bean) throws SQLException;
+
+    boolean delete(TYPE bean) throws SQLException;
+
+    List<TYPE> getAll() throws SQLException;
+}
