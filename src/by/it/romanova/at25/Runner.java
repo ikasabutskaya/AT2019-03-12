f5f2ab16c73bb783e622fef6e527edc4a9503561
Liza <>
2019-05-16 23:51:37 +0300

    at25 A add

--- src/by/it/romanova/at25/Runner.java
+++ src/by/it/romanova/at25/Runner.java
@@ -1,14 +1,27 @@
 package by.it.romanova.at25;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Runner {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        List<Buyer> buyers = new ArrayList<>();
+
         int buyercounter = 0;
         for (int i = 0; i < 5; i++) {
            // Util.sleep(1000);
             Buyer buyer = new Buyer(buyercounter);
+            buyers.add(buyer);
             buyer.start();
             buyercounter++;
         }
+
+        for (Buyer buyer : buyers) {
+            buyer.join();
+        }
+
+
+        System.out.println("Market closed");
 
     }
 
