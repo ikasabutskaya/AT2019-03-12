72ab55fa5503f2defbc7b9abbf62ac6731e735d3
HPG4 <sa.simanovich@gmail.com>
2019-05-22 18:42:10 +0300

    v12

--- projects/simanovich/hellowebdriver/src/test/java/by/it/simanovich/CheckFindImageGoogleSiteTest.java
+++ projects/simanovich/hellowebdriver/src/test/java/by/it/simanovich/CheckFindImageGoogleSiteTest.java
@@ -5,11 +5,20 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import static junit.framework.TestCase.assertTrue;
 
 public class CheckFindImageGoogleSiteTest {
 
---
+++ src/by/it/simanovich/at25/IBuyer.java
@@ -0,0 +1,9 @@
+package by.it.simanovich.at25;
+
+public interface IBuyer {
+
+    void enterToMarket();
+    void chooseGoods();
+    void goOut();
+
+}
