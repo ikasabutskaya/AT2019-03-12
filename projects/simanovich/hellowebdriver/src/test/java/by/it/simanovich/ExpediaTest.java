15a887bc2c2f8994961308f3e1bcde1c6fb43d0d
HPG4 <sa.simanovich@gmail.com>
2019-05-23 13:24:34 +0300

    commit

--- projects/simanovich/hellowebdriver/src/test/java/by/it/simanovich/ExpediaTest.java
+++ projects/simanovich/hellowebdriver/src/test/java/by/it/simanovich/ExpediaTest.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeMethod;
 
 public class ExpediaTest {
 
