03a32af20da54b62c0abd3ddc69afd72367603b8
TatsianaTsyhanova <tatsianatsyhanova@yahoo.com>
2019-05-24 16:23:18 +0300

    helloWebDriver PageObject

--- projects/tsyhanova/hellowebdriver/src/test/java/by/it/tsyhanova/ExpediaTest.java
+++ projects/tsyhanova/hellowebdriver/src/test/java/by/it/tsyhanova/ExpediaTest.java
@@ -1,19 +1,18 @@
 package by.it.tsyhanova;
 
+import by.it.tsyhanova.pages.ResultPage;
+import by.it.tsyhanova.pages.StartPage;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.time.LocalDateTime;
 
 public class ExpediaTest {
 
-
     WebDriver driver;
+
 
     @Before
     public void setUpBrowser() {
@@ -22,9 +21,7 @@ public class ExpediaTest {
 
     @Test
     public void taskA() throws Exception {
-
-
-        /*driver.get("https://www.expedia.com/");
+        driver.get("https://www.expedia.com/");
 
         StartPage startPage = new StartPage(driver);
 
@@ -37,48 +34,17 @@ public class ExpediaTest {
                 .getSearch();
 
         int resultSearchCount = resultPage.getResultSearchCount();
-        Assert.assertTrue(resultSearchCount > 1);*/
-
-
-        driver.get("https://www.expedia.com/");
-        Thread.sleep(5000);
-        WebElement buttonFlight=driver.findElement(By.id("tab-flight-tab-hp"));
-        buttonFlight.click();
-        Thread.sleep(5000);
-        WebElement oneWay=driver.findElement(By.id("flight-type-one-way-label-hp-flight"));
-        oneWay.click();
-        Thread.sleep(10000);
-        WebElement inputFlightingFrom=driver.findElement(By.id("flight-origin-hp-flight"));
-        inputFlightingFrom.click();
-        inputFlightingFrom.sendKeys("Minsk, Belarus (MSQ-All Airports)");
-        Thread.sleep(10000);
-        WebElement inputFlightingTo=driver.findElement(By.id("flight-destination-hp-flight"));
-        inputFlightingTo.click();
-        inputFlightingTo.sendKeys("Moscow, Russia (MOW-All Airports)");
-        Thread.sleep(10000);
-        WebElement inputDepartingDate=driver.findElement(By.id("flight-departing-hp-flight"));
-        inputDepartingDate.click();
-        LocalDateTime now=LocalDateTime.now();
-        int year=now.getYear();
-        int month=now.getMonthValue();
-        int day=now.getDayOfMonth();
-        StringBuilder currentDate=new StringBuilder();
-        String currentYear=Integer.toString(year);
-        String currentMonth=Integer.toString(month);
-        String currentDay=Integer.toString(day);
-        currentDate.append(currentMonth);
-        currentDate.append("/");
-        currentDate.append(currentDay);
-        currentDate.append("/");
-        currentDate.append(currentYear);
-        System.out.println(currentDate);
-        inputDepartingDate.sendKeys(currentDate);
-        Thread.sleep(10000);
+        Assert.assertTrue(resultSearchCount > 1);
     }
+    @Test
+    public void taskB(){
+        driver.get("https://www.expedia.com/");
 
+
+    }
 
     @After
     public void tearDownBrowser() {
-       // driver.quit();
+        driver.quit();
     }
 }
