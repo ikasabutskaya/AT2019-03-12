8fc5740ceff3bf0597276097b8ecf1c0b91ab9ed
ikasabutskaya <ikasabutskaya@gmail.com>
2019-05-27 02:02:59 +0300

    at25, lvl a

---
+++ src/by/it/ikasabutskaya/at25/Dispatcher.java
@@ -0,0 +1,9 @@
+package by.it.ikasabutskaya.at25;
+
+class Dispatcher {
+
+    static final int K_SPEED=100;
+
+    static int buyerCounter=0;
+
+}
---
+++ src/by/it/ikasabutskaya/at25/Util.java
@@ -0,0 +1,29 @@
+package by.it.ikasabutskaya.at25;
+
+import java.util.Random;
+
+class Util {
+
+    private static Random rnd=new Random();
+
+    private Util() {
+    }
+
+    static int random(int start, int stop) {
+        int delta=(stop - start);
+        return rnd.nextInt(delta)+start;
+    }
+
+    static int random(int max) {
+        return random(0, max);
+    }
+
+    static void sleep(int timeout) {
+        try {
+            Thread.sleep(timeout/ Dispatcher.K_SPEED);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
