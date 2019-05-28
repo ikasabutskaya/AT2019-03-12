c0b07edb127893414a4a4aced0f1f40b7d4432a6
okoyro <oksana.koyro@gmail.com>
2019-05-28 12:03:34 +0300

    at_23 taskB

--- projects/okoyro/hellowebdriver/src/test/java/by/it/okoyro/CheckSeleniumSiteTest.java
+++ projects/okoyro/hellowebdriver/src/test/java/by/it/okoyro/CheckSeleniumSiteTest.java
@@ -1,14 +1,15 @@
 package by.it.okoyro;
 
-import org.junit.Assert;
-import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CheckSeleniumSiteTest {
@@ -17,12 +18,10 @@ public class CheckSeleniumSiteTest {
 		WebDriver driver = new ChromeDriver();  // use @Before annotation
 		try {
 			driver.get("https://google.com");
-			WebElement firstElement =(new WebDriverWait(driver, 5))
+			WebElement searchInput = (new WebDriverWait(driver, 5))
 					.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input.gLFyf.gsfi")));
-			firstElement.sendKeys("seleniumhq");
-			firstElement.submit();
-//			driver.findElement(By.xpath("//input[@class = 'gLFyf gsfi']")).sendKeys("seleniumhq");
-//			driver.findElement(By.xpath("//input[@class = 'gLFyf gsfi']")).submit();
+			searchInput.sendKeys("seleniumhq");
+			searchInput.submit();
 			(new WebDriverWait(driver, 5))
 					.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[@class='csb ch']")));
 			driver.findElements(By.className("LC20lb")).get(0).click();
@@ -31,15 +30,20 @@ public class CheckSeleniumSiteTest {
 
 			driver.findElement(By.cssSelector("li#menu_download>a")).click();
 			(new WebDriverWait(driver, 5))
-								.until(ExpectedConditions.presenceOfElementLocated(By.className("sectionDownload")));
+					.until(ExpectedConditions.presenceOfElementLocated(By.className("sectionDownload")));
 			driver.findElements(By.xpath("//*[@id='nav']//a[text()='Maven Information']")).get(0).click();
 			(new WebDriverWait(driver, 5))
-											.until(ExpectedConditions.textMatches(By.cssSelector("#mainContent h2"),
-																				  Pattern.compile("Maven Information")));
+					.until(ExpectedConditions.textMatches(By.cssSelector("#mainContent h2"),
+														  Pattern.compile("Maven Information")));
 			String dependencyText = driver.findElements(By.tagName("pre")).get(0).getText();
 			Assert.assertTrue(dependencyText.contains("<artifactId>selenium-java</artifactId>"));
-//			dependencyText.split("<version>(.*)<\\/version>"); add pattern/matcher regexp
-
+			// dependency version
+			Pattern versionPattern = Pattern.compile("<version>(.*)<\\/version>");
+			Matcher matcher = versionPattern.matcher("<dependency>         <groupId>org.seleniumhq.selenium</groupId>         <artifactId>selenium-java</artifactId>         <version>3.141.59</version>     </dependency>");
+			if (matcher.find()) {
+				String version = matcher.group(1);
+				System.out.println(version);
+			}
 		}
 		finally {    // use @After annotation
 			driver.close();
