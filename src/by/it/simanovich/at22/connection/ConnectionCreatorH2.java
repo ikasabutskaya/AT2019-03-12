fca7914c96113b1165f3ce725d2e634e5d6b1eda
HPG4 <sa.simanovich@gmail.com>
2019-05-29 19:10:28 +0300

    v12

---
+++ src/by/it/simanovich/at22/connection/ConnectionCreatorH2.java
@@ -0,0 +1,26 @@
+package by.it.simanovich.at22.connection;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionCreatorH2 implements ConnectionCreator{
+
+    static {
+        URL resource = ConnectionCreatorH2.class.getClassLoader().getResource("asimanovich.sql");
+        String sqlFile=resource.toString();
+        try {
+            DriverManager.getConnection("" +
+                    "jdbc:h2:mem:test;" +
+                    "INIT=RUNSCRIPT FROM '"+sqlFile+"'");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Connection get() throws SQLException {
+        return DriverManager.getConnection("jdbc:h2:mem:test");
+    }
+}
