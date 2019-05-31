6b882d4e5566643a50e1c0511bb0080ca8c701ce
agrinkevich <grinkevich.alina@gmail.com>
2019-05-31 13:18:24 +0300

    test commit 2

--- projects/agrinkevich/hellowebdriver/src/test/java/by/it/agrinkevich/cucumber/BddAirLineTest.java
+++ projects/agrinkevich/hellowebdriver/src/test/java/by/it/agrinkevich/cucumber/BddAirLineTest.java
@@ -4,7 +4,6 @@ package by.it.agrinkevich.cucumber;
 import cucumber.api.CucumberOptions;
 import cucumber.api.SnippetType;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
-import org.testng.annotations.Test;
 
 @CucumberOptions(
         features = "src/test/java/by/it/agrinkevich/cucumber",
@@ -12,7 +11,6 @@ import org.testng.annotations.Test;
         snippets = SnippetType.CAMELCASE
 )
 
-@Test
 public class BddAirLineTest extends AbstractTestNGCucumberTests {
 }
 
